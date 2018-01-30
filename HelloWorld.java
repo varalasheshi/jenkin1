d539308ea3815612d8fd809a6f4a23e143fd7b05
varalasheshi <35951160+varalasheshi@users.noreply.github.com>
2018-01-30 17:05:03 +0530

    Update HelloWorld.java
    added success

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,8 +6,8 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Testing");
-		System.out.println("Testing111111111");
+		System.out.println("Testing-success");
+		
 	}
 
 }
